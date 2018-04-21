9deb0dfec6e9f5390769946ae28423aab2c1f337
dredscabral <andrade.andreaa@gmail.com>
2018-04-21 17:37:39 -0400

    Explosion animation

---
+++ Bomberman/Assets/Scripts/BompDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BompDrop : MonoBehaviour {
+
+    public GameObject bombPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space)))
+        {
+
+            Vector2 pos = transform.position;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+
+            Instantiate(bombPrefab, pos, Quaternion.identity);
+        }
+    }
+}
